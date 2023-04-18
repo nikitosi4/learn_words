ae9ec33e192f90069ee9178af10a62f07aef00d2
nikitosi4 <kitvladimirovi4@gmail.com>
2023-04-19 02:50:43 +0300

    added deletion of words from the database

--- core.py
+++ core.py
@@ -19,13 +19,40 @@ class AddWord(Tk):
     def get_word(self):
         en = self.eng_word.get()
         ru = self.ru_word.get()
-        print([en, ru])
         return [en, ru]
 
     def save_data(self):
         value = self.get_word()
         cur.execute("INSERT INTO words(eng_word, ru_word) values(?,?);", value)
         db.commit()
+        AddWord.destroy(self)
+
+
+class DictWin(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Словарь")
+        cur.execute("SELECT * FROM words;")
+        self.all_results = cur.fetchall()
+        if len(self.all_results) == 0:
+            Label(self, text="Словарь пуст. Внесите хотябы одну пару слов и тогда они появятся здесь!!!").grid(row=0, column=0)
+        else:
+            for i in range(len(self.all_results)):
+                x = i
+                Label(self, text=self.all_results[i][0]).grid(row=i, column=0)
+                Label(self, text=self.all_results[i][1]).grid(row=i, column=1)
+                self.make_del_btn(i).grid(row=i, column=2)
+
+    def make_del_btn(self, x):
+        return tk.Button(self, text="Удалить", command=lambda:self.del_word(x))
+
+    def del_word(self,x):
+        word = self.all_results[x][0]
+        print(word)
+        cur.execute("DELETE FROM words WHERE eng_word=?", [word])
+        db.commit()
+        DictWin.destroy(self)
+        DictWin()
 
 
 db = sl.connect("DataBase.db")
@@ -42,8 +69,10 @@ win.title("Запоминалка слов")
 win.config(bg="black")
 
 
-add_btn = tk.Button(win, text="Добваить новое слово", command=lambda: AddWord())
+add_btn = tk.Button(win, text="Добваить новое слово", command=AddWord)
 add_btn.pack()
+dict_btn = tk.Button(win, text="Словарь", command=DictWin)
+dict_btn.pack()
 
 
 win.mainloop()
